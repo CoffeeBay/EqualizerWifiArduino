b929d35b6fdb25c4f0e147229982c4c75f84c143
BangUl_Pro <wind.dong.dream@gmail.com>
2016-11-20 00:25:52 +0900

    Serial 통신

--- equalizerWifi/equalizerWifi.ino
+++ equalizerWifi/equalizerWifi.ino
@@ -1,10 +1,40 @@
+char receivedData[10];
+int count = 0;
+int command = 0;
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  
+  delay(1000);
+  Serial.println("start1");
 }
 
 void loop() {
   if (Serial.available()) {
-    Serial.print("데이터 수신 : ");
-    Serial.write(Serial.read());
+    char c = Serial.read();
+
+    if (c >= 'a' && c <= 'z') {
+      receivedData[count++] = c;
+    }
+
+    if (count == 2) {
+      // play
+      if (receivedData[0] == 'p' && receivedData[1] == 'l') {
+        Serial.println("play");
+      }
+      // next
+      if (receivedData[0] == 'n') {
+        Serial.println("next");
+      }
+      // stop
+      if (receivedData[0] == 's') {
+        Serial.println("stop");
+      }
+      // prev
+      if (receivedData[0] == 'p' && receivedData[1] == 'r') {
+        Serial.println("prev");
+      }
+      count = 0;
+    }
   }
 }
