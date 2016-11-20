76fa22b92f1fd12d1f22576879c417936bbf9340
BangUl_Pro <wind.dong.dream@gmail.com>
2016-11-20 13:18:57 +0900

    명령어 단순화 및 가사 아두이노로 데이터 전송

--- equalizerWifi/equalizerWifi.ino
+++ equalizerWifi/equalizerWifi.ino
@@ -1,40 +1,52 @@
-char receivedData[10];
-int count = 0;
+char receivedData;
 int command = 0;
+int inputStatus = -1;
 
 void setup() {
   Serial.begin(115200);
   
   delay(1000);
-  Serial.println("start1");
+  Serial.println("Arduino Start!");
 }
 
 void loop() {
   if (Serial.available()) {
     char c = Serial.read();
+    Serial.write(c);
 
-    if (c >= 'a' && c <= 'z') {
-      receivedData[count++] = c;
+    if (c == '@') {
+      inputStatus = 1;
     }
 
-    if (count == 2) {
-      // play
-      if (receivedData[0] == 'p' && receivedData[1] == 'l') {
-        Serial.println("play");
+    // 와이파이에서 들어온 명령 ex)@a, @b, @c, @d
+    else if (inputStatus) {
+      // play index 받을 때
+      if (command == 1) {
+        command = 0;
+        inputStatus = 0;
+        // c == index
+        Serial.print("play ");
+        Serial.print(inputStatus);
       }
-      // next
-      if (receivedData[0] == 'n') {
-        Serial.println("next");
+      // play
+      else if (c == 'a') {
+        command = 1;
       }
       // stop
-      if (receivedData[0] == 's') {
-        Serial.println("stop");
+      else if (c == 'b') {
+        command = 0;
+        inputStatus = 0;
+      }
+      // next
+      else if (c == 'c') {
+        command = 0;
+        inputStatus = 0;
       }
       // prev
-      if (receivedData[0] == 'p' && receivedData[1] == 'r') {
-        Serial.println("prev");
+      else if (c == 'd') {
+        command = 0;
+        inputStatus = 0;
       }
-      count = 0;
     }
   }
 }
